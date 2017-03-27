00e5cb1f5a09f673650824df867441ee85984f2c
BenedikteEva <cph-bm109@cphbusiness.dk>
2017-03-27 15:22:11 +0200

    text adventure gittest

--- TagVer3/TagVer3/src/tagver3/PrettyDolls.java
+++ TagVer3/TagVer3/src/tagver3/PrettyDolls.java
@@ -10,23 +10,46 @@ package tagver3;
  * @author Ejer
  */
 public class PrettyDolls implements Items   {
+
+    
+   
+    String name;
     String description;
-    int destress;
+    int stressrelease;
+    
+    
+ public PrettyDolls(String name, String description, int stressrelease) {
+        this.name = name;
+        this.description = description;
+        this.stressrelease = stressrelease;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getStressrelease() {
+        return stressrelease;
+    }
+
+    public void setStressrelease(int stressrelease) {
+        this.stressrelease = stressrelease;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     
 
-    @Override
-    public void useItem() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void pickItem() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public void throwItem() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+  
     
 }

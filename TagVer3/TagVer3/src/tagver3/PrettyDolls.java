e2b0be9a432b4575bc4c44490f9f5e7becd00cd9
BenedikteEva <cph-bm109@cphbusiness.dk>
2017-03-27 10:41:44 +0200

    text adventure gittest

---
+++ TagVer3/TagVer3/src/tagver3/PrettyDolls.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tagver3;
+
+/**
+ *
+ * @author Ejer
+ */
+public class PrettyDolls implements Items   {
+    String description;
+    int destress;
+    
+
+    @Override
+    public void useItem() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void pickItem() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void throwItem() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}

8795998a298e1bc11c13c78ef0fa63c6663485ab
BenedikteEva <cph-bm109@cphbusiness.dk>
2017-03-28 13:21:30 +0200

    text adventure gittest

--- TagVer3/TagVer3/src/tagver3/Items.java
+++ TagVer3/TagVer3/src/tagver3/Items.java
@@ -1,19 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package tagver3;
+
+import java.util.ArrayList;
 
 /**
  *
  * @author Zack
  */
+
+// rygsæk indeholder Items og fælles metoder
 public interface Items {
+
+  
     
     public void useItem();
+    public void  pickItem();
+    public void getItem();
     
-    public void pickItem();
-    
-    public void throwItem();
 }
